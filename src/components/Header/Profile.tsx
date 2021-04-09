f21fadcfad19b840ac851f41bba73ea7bc3c257c
Joao Garcia <jvdasilvaeirasgarcia@gmail.com>
2021-04-09 09:33:18 -0300

    adapted for mobile

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,19 +1,24 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex
       align="center"
     >
-      <Box mr="4" textAlign="right">
-        <Text>João Vitor Garcia</Text>
-        <Text color="gray.300" fontSize="small">
-          jvdasilvaeirasgarcia@gmail.com
-        </Text> 
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>João Vitor Garcia</Text>
+          <Text color="gray.300" fontSize="small">
+            jvdasilvaeirasgarcia@gmail.com
+          </Text> 
+        </Box>
+      )}
 
       <Avatar size="md" name="João Garcia" />
     </Flex>
   );
 };
-  
